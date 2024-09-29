2edc7b320700c0b9c93a8869497252590e6ded72
Sridhar S Shenoy <sridhar.shenoy@gmail.com>
2024-09-29 14:28:25 +0800

    Fixed the error

--- src/main/resources/webapp/App.js
+++ src/main/resources/webapp/App.js
@@ -43,9 +43,43 @@ document.addEventListener('DOMContentLoaded', () => {
                 ${Object.keys(data).map(key => `
                     <div class="tab-pane fade" id="${key}">
                         <h5>${key} Data</h5>
-                        <pre>${JSON.stringify(data[key], null, 2)}</pre>
+                        ${key === 'Actions' ? renderActionsTable(data[key]) : `<pre>${JSON.stringify(data[key], null, 2)}</pre>`}
                     </div>`).join('')}
             </div>
+        `;
+    }
+
+    function renderActionsTable(actions) {
+        return `
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>System</th>
+                        <th>Log</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${actions.map((action, index) => {
+                        const logSnippet = action.log ? action.log.substring(0, 50) : "No log data available";
+                        return `
+                        <tr class="collapse-row" data-target="#jsonRow${index}" data-toggle="collapse">
+                            <td>${action.system || "Unknown"}</td>
+                            <td>${logSnippet}...</td>
+                        </tr>
+                        <tr class="collapse" id="jsonRow${index}">
+                            <td colspan="2">
+                                <table class="table json-table">
+                                    ${Object.keys(action).map(key => `
+                                        <tr>
+                                            <td><strong>${key}</strong></td>
+                                            <td>${JSON.stringify(action[key], null, 2)}</td>
+                                        </tr>`).join('')}
+                                </table>
+                            </td>
+                        </tr>`;
+                    }).join('')}
+                </tbody>
+            </table>
         `;
     }
 });
